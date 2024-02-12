a9f13e650debc60f3549064ad6a8359f44f930bd
Martin Morales <themartin93@gmail.com>
2024-02-11 23:21:52 -0600

    Netlify adapter added
    This adapter is added because `[NoAdapterInstalled]
    Cannot use `output: "server"` or `output: "hybrid"`
    without an adapter. Please install and configure the
    appropriate server adapter for your final deployment.`.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  output: "hybrid"
+  output: "hybrid",
+  adapter: netlify()
 });
