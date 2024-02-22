9e449ba3c2b310d3eeaec5119ff9e1e3c00ca24f
Martin Morales <themartin93@gmail.com>
2024-02-21 23:45:08 -0600

    Added post content to rss
    Added plugins: html-sanitize and markdown it

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,8 @@
 import rss from '@astrojs/rss';
 import { getCollection } from "astro:content";
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
 
@@ -13,7 +16,7 @@ export async function GET(context) {
             title: post.data.title,
             pubDate: post.data.date,
             link: `/${post.slug}`,
-            description: post.body
+            content: sanitizeHtml(parser.render(post.body)),
         })),
         customData: `<language>en-us</language>`,
     });
