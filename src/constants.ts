5d5d39510bc27cb424a9fd985d1ad9614d47fa1c
Martin Morales <themartin93@gmail.com>
2024-03-01 18:47:02 -0600

    RSS improvements
    A new function was added to clean and format post's bodies, also this function is placed in a separate file for better readibility.
    Markdown plugin was used and configured again to convert markdown syntax into html code.
    
    Some posts were modified to use markdown syntax.

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+export const URL_SITE = "https://mrtnmrls.com";
+export const RSS = {
+    title: 'Martin Morales',
+    description: 'A small collection of thoughts, experiences and other things',
+    site: URL_SITE,
+    copyright: "Copyright 2024, Martin Morales",
+    managingEditor: "isthemartin@gmail.com (Martin Morales)",
+    webMaster: "isthemartin@gmail.com (Martin Morales)",
+    generator: "Netlify + Astro",
+    docs: 'https://www.rssboard.org/rss-specification',
+    ttl:1440,
+    language: '<language>en-us</language>'
+};
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,26 +1,32 @@
 import rss from '@astrojs/rss';
 import { getCollection } from "astro:content";
-import sanitizeHtml from 'sanitize-html';
-import MarkdownIt from 'markdown-it';
-
-// JSX components will not be detected in MarkdownIt
-const markdownParser = new MarkdownIt();
+import { RSS } from 'src/constants';
+import { formatBodyForRSS } from 'src/functions';
 
 export async function GET(context) {
 
     const posts = await getCollection('posts');
+    posts.sort((a, b) => Date.parse(b.data.pubDate.toString()) - Date.parse(a.data.pubDate.toString()));
+    const lastPubDate = posts[0].data.pubDate
 
     return rss({
-        title: 'Martin Morales',
-        description: 'A small collection of thoughts, experiences and other things.',
-        site: context.site,
+        title: RSS.title,
+        description: RSS.description,
+        site: RSS.site,
+        copyright: RSS.copyright,
+        managingEditor: RSS.managingEditor,
+        webMaster: RSS.webMaster,
+        lastBuildDate: lastPubDate,
+        generator: RSS.generator,
+        docs: RSS.docs,
+        ttl: RSS.ttl,
         items: posts.map((post) => ({
             title: post.data.title,
             author: post.data.author,
             pubDate: post.data.pubDate,
             link: `/${post.slug}`,
-            description: `<![CDATA[${sanitizeHtml(markdownParser.render(post.body))}]]>`,
+            description: formatBodyForRSS(post.body),
         })),
-        customData: `<language>en-us</language>`,
+        customData: RSS.language,
     });
 }
